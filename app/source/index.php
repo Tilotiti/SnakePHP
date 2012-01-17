94641dc34bbc5f5e512d1f864fed260f05e622bb
Tilotiti <thibaulthenry@tilotiti.com>
2012-01-17 16:16:08 +0100

    Class Query Alias
    Suppression des tests sur l'index.

--- app/source/index.php
+++ app/source/index.php
@@ -4,20 +4,4 @@ $page->pushSidebar('test', 'left');
 $page->pushSidebar('test', 'left');
 $page->pushSidebar('test', 'right');
 $page->pushSidebar('test', 'right');
-
-$query = new query();
-$query->select(array('DISTANCE' => array(
-		  	"lat",
-		  	"long",
-		  	 48.882578,
-		  	 2.2893605
-		  ),'AS' => "distance"),
-		  "username"
-	)
-	->from('user')
-	->orderBy(array('ALLIAS' => 'distance'), "ASC")
-	->exec('ALL');
-	
-$query->debug();
-debug::display($query->getArray(), "Résultats de la requête");
 ?>
