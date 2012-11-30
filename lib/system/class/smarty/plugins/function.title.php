c7f398210b7cf1e097e49b61c65a9637ccbca29a
Thibault Henry <thibaulthenry@tilotiti.com>
2012-11-30 15:12:36 +0100

    Update smarty function title
    Add var support for the lang class

--- lib/system/class/smarty/plugins/function.title.php
+++ lib/system/class/smarty/plugins/function.title.php
@@ -1,7 +1,11 @@
 <?
 function smarty_function_title($params, $template) {
 	if(isset($params['code'])):
-		return lang::title($params['code']);
+		if(isset($params['var'])):
+			return lang::title($params['code'], $params['var']);
+		else:
+			return lang::title($params['code']);
+		endif;
 	endif;
 }
 
