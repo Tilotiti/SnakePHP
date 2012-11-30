9fa782f6a19d7696ad46374687b4e4e8b4993618
Thibault Henry <thibaulthenry@tilotiti.com>
2012-11-30 15:10:57 +0100

    Update smarty function error
    Add variable support for the lang class

--- lib/system/class/smarty/plugins/function.error.php
+++ lib/system/class/smarty/plugins/function.error.php
@@ -1,7 +1,11 @@
 <?
 function smarty_function_error($params, $template) {
 	if(isset($params['code'])):
-		return lang::error($params['code']);
+		if(isset($params['var'])):
+			return lang::error($params['code'], $params['var']);
+		else:
+			return lang::error($params['code']);
+		endif;
 	endif;
 }
 
