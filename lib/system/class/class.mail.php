f2b838bbc70407fa26b6a4bb3fd11b64e2906240
Thibault Henry <thibaulthenry@tilotiti.com>
2013-05-15 11:48:53 +0200

    Update class mail
    Send strict mime-type mail

--- lib/system/class/class.mail.php
+++ lib/system/class/class.mail.php
@@ -13,7 +13,6 @@ class mail {
         $limite   = false;
 
     public function __construct() {
-        
     }
 
     public function to($mail) {
@@ -41,13 +40,12 @@ class mail {
         return $this;
     }
 
-    public function subject($str, $lang = true) {
-        if($lang){
-            $this->subject = lang::title($str);
-        }
-        else{
+    public function subject($str, $lang = true, $encode = false) {
+        if($lang):
+            $this->subject = lang::title($str, $lang);
+        else:
             $this->subject = $str;
-        }
+        endif;
         return $this;
     }
 
@@ -60,12 +58,11 @@ class mail {
     }
 
     public function message($str, $arg = false, $lang = true) {
-        if($lang){
+        if($lang):
             $this->message = lang::mail($str, $arg);
-        }
-        else{
+        else:
             $this->message = $str;
-        }
+        endif;
         return $this;
     }
 
@@ -103,84 +100,32 @@ class mail {
             $return = '';
             foreach($array as $key => $value):
                 if(!empty($value)):
-                    $return .=  $key.': '.$value."\r\n";
+                    $return .=  $key.': '.$value."\n";
                 endif;
             endforeach;
             return $return;
         endif;
     }
-    
-    /**
-     * Converts the body message into multipart/alternative message.
-     * Replace links with footnotes.
-     * @return the multipart/alternative mail body
-     */
-    public function convertMessage() {
-        
-        // replace all <br(?)> with nl and enclose each <p></p> with nl
-        $texte = preg_replace('#<br[^>]*>#', "\r\n", $this->message);
-        $texte = preg_replace('#<p[^>]*>#', "\r\n", $texte);
-        $texte = preg_replace('#</p[^>]*>#', "\r\n", $texte);
-        // get all href="???"
-        $hrefs = array();
-        preg_match_all('#<a [^>]*href="?([^">]+)"?>#', $texte, $hrefs);
-        $hrefs = $hrefs[1];
-        // replace all <axxx> and </axxx>
-        $texte = preg_replace('#<a[^>]*>#', '', $texte);
-
-        // add a mark for each end of link
-        $texte = preg_replace('#</a[[:space:]]*>#', '[href] ', $texte);
-
-        $texte = strip_tags($texte);
-
-        $i = 1;
-        while ( ($pos=strpos($texte, '[href]'))!==false ) { // give a note number
-            $texte = substr($texte, 0, $pos) . '['. $i++ .']' . substr($texte, $pos+6);
-        }
-        foreach($hrefs as $i => $link) { // adding footnotes
-            $texte .= '[' . (1+$i++) . '] ' . $link."\r\n";
-        }
-        // --> mail body in plain text is now ready <--
-        
-        $br      = "\r\n";
-        $message = "";
-        
-        // adding plain text message header
-        $message .= $br."--".$this->limite.$br;
-        $message .= 'Content-Type: text/plain; charset="'.CHARSET.'"'.$br;
-        $message .= "Content-Transfer-Encoding: 8bit".$br;
-        $message .= $br.$texte.$br;
-        
-        // html message header
-        $message .= $br."--".$this->limite.$br;
-        $message .= 'Content-Type: text/html; charset="'.CHARSET.'"'.$br;
-        $message .= "Content-Transfer-Encoding:8bit;"."\r\n";
-        $message .= $br.$this->message.$br;
-        
-        $message .= $br."--".$this->limite."--".$br;
-        $message .= $br."--".$this->limite."--".$br;
-        
-        return $message;
-    }
-
 
     public function send() {
-        $this->limite = "-----=".md5(rand());
         $this->header("Bcc", $this->convertmail($this->bcc));
         $this->header("Cc", $this->convertmail($this->cc));
         $this->header("Reply-To", $this->convertmail($this->replyto));
         $this->header("X-Confirm-Reading-To", $this->convertmail($this->confirm));
         $this->header("MIME-Version", "1.0");
-        $this->header("Content-type", 'multipart/alternative;'."\r\n".' boundary="'.$this->limite.'"');
+        $this->header("Content-type", 'text/html; charset="'.CHARSET.'"');
 
         $headers = $this->convertheader($this->header);
-        $message = $this->convertMessage();
-        
+
         $this->subject = mb_encode_mimeheader($this->subject, CHARSET, "Q");
-                
+
+        $return = true;
+
         foreach($this->to as $to):
-            mail($to, $this->subject, $message, $headers);
+            $return = $return && mail($to, $this->subject, $this->message, $headers);
         endforeach;
+
+        return $return;
     }
 }
 ?>
