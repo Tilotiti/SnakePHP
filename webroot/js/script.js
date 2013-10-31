8b19dc39d8fde19316842bf86e8fa6b6b05fb9c7
Tilotiti <thibaulthenry@tilotiti.com>
2013-10-31 12:29:52 +0100

    JS
    - Session save
    - Icone simplifier
    - Deleting message class

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -2,7 +2,6 @@ $(function() {
 	/*
 	 * Debug Bar
 	 */
-	 
 	var navBar = ["Error", "SQL", "Dump", "Global", "Timer"];
 	
 	$.each(navBar, function(i, e) {
@@ -25,11 +24,9 @@ $(function() {
 		 	return false;
 		});
 	});
-});
-
-$(window).load(function () {
+	
 	/*
-	 * Automatic image croping - TODO test
+	 * Automatic image croping
 	 */
 	$('.cropbox').each(function(i, e) {
 		
@@ -49,75 +46,18 @@ $(window).load(function () {
 	});
 });
 
-var Message = function(){};
-
-/**
- * push(type[string], content[string], cfg[object])
- *
- * @var type : type de message (success, alert, ...)
- * @var content : le message en lui-même
- * @var cfg : tableau de configuration (optionel) permetant de gérer :
- * 	- la cible (pour l'integration du message)
- * 	- le delai avant la disparition du message
- * 	- la durée de l'effet de fondue
- * 
- * example d'utilisation simple :
- * 	pushMessage('success', 'Modification réalisée avec succès');
- * 
- * example d'utilisation avec configuration :
- * 	pushMessage('success', 'Modification réalisée avec succès', {
- * 		delay : 2000, // 2sec
- * 		fade : 750
- * 	});
+/*
+ * Icone simplifier
  */
-Message.prototype.push = function(type, content, cfg){
-	
-	if(typeof type == 'undefined'){
-		return false;
-	}
-	if(typeof content == 'undefined' || content == ''){
-		return false;
-	}
-
-	var alert 	= '<div class="alert '+type+'">'+content+'</div>';
- 	var target 	= $('#module_content h2:eq(0)');
- 	var delay 	= 10000;
- 	var fade 	= 2500;
-
- 	if(typeof cfg == 'object'){
- 		delay 	= typeof cfg.delay  == 'undefined' ? delay 	: cfg.delay;
- 		fade 	= typeof cfg.fade   == 'undefined' ? fade 	: cfg.fade;
- 		target 	= typeof cfg.target == 'undefined' ? target : cfg.target;
- 	}
-
-
- 	var newItem = $(alert).insertAfter(target);
-
- 	if(newItem){
- 		$(newItem).delay(delay).fadeOut(fade);
- 	}
+function icone(str) {
+	return '<i class="icon-'+str+'"></i>'
 }
 
-/**
- * Message.success(content[string], cfg[object])
- * Simple alias de la fonction Message.push avec la variable type égale à 'success'
- * 
- * @var content : le message en lui-même
- * @var cfg : tableau de configuration (optionel) permetant de gérer :
- * 	- la cible (pour l'integration du message)
- * 	- le delai avant la disparition du message
- * 	- la durée de l'effet de fondue 
+/*
+ * Session auto-save
  */
-Message.prototype.success = function(content, cfg){
-	this.push('success', content, cfg);
+ 
+setInterval(sessionSave, 600000);
+function sessionSave() {
+	$.get('/ajax/session.php');
 }
-
-/**
- * Message.success(content[string], cfg[object])
- * Simple alias de la fonction Message.push avec la variable type égale à 'error'
- */
-Message.prototype.error = function(content, cfg){
-	this.push('error', content, cfg);
-}
-
-message = new Message();
