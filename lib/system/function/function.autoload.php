6318718ce7222a3081faa93dbbc0e86c94cc72fd
Fabrice Lazzarotto <fl@digitaldealer.fr>
2013-12-12 14:44:28 +0100

    now autoload can deal with lowercase dirs/file plugin names

--- lib/system/function/function.autoload.php
+++ lib/system/function/function.autoload.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+ * Autoload function
+ * Whenever an unknown class/function is invoked, this function is called to include correct file(s)
+ * Outputs an error if resource not found
+ * 
+ * @param String $class class or function name to invoke
+ * @return void
+ */
 function autoload($class) {
     if ($class == "smarty" || !preg_match("#smarty#i", $class)):
 	    if (file_exists(PLUGIN.'/'.$class.'/plugin.'.$class.'.php')):
 	    	require_once PLUGIN.'/'.$class.'/plugin.'.$class.'.php';
+		elseif (file_exists(PLUGIN.'/'.strtolower($class).'/plugin.'.strtolower($class).'.php')):
+	    	require_once PLUGIN.'/'.strtolower($class).'/plugin.'.strtolower($class).'.php';
 	    elseif(file_exists(SYSTEM.'/class/class.'.$class.'.php')):
 	        require_once SYSTEM.'/class/class.'.$class.'.php';
 		elseif (preg_match('#^swift_#i', $class)):
