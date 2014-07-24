a323fabe662eae184a33be9aa4e3fba6621c6f7e
Thibault Henry <thibaulthenry@tilotiti.com>
2014-07-24 12:41:20 +0200

    Refactoring plugin CSRF

--- lib/plugin/csrf/plugin.csrf.php
+++ lib/plugin/csrf/plugin.csrf.php
@@ -2,13 +2,52 @@
 /*
  * Plugin : CSRF
  * Description : Protects website from "Cross-site request forgery" attacks
- * Autor : Pierre de Vésian
- * Site : https://twitter.com/pvesian
+ * Autor : Thibault HENRY
+ * Site : http://www.tiloweb.com
 */
 
-// Load the CSRF class
-require_once 'class.csrf.php';
-
-/*
- * FYI: CSRF-token are built by two parameters: their NAME and their VALUE
- */
+class csrf {
+	/**
+	 * Generate
+	 * Generating a unique CSRF token
+	 * @param string $name Form name (optional)
+	 */
+	static function generate($name = 'default') {
+		if(!isset($_SESSION['csrf'])):
+			$_SESSION['csrf'] = array();
+		endif;
+		
+		$_SESSION['csrf'][$name] = sha1($name.time().rand(1,999).$_SERVER['REMOTE_ADDR']);
+	}
+	
+	/**
+	 * Display
+	 * Display the token input in form
+	 * @param string $name Form name (optional)
+	 */
+	static function display($name = 'default') {
+		if(!isset($_SESSION['csrf'][$name])):	
+			return false;
+		endif;
+		
+		return '<input type="hidden" value="'.$_SESSION['csrf'][$name].'" name="CSRFtoken_'.$name.'">';
+	}
+	
+	/**
+	 * Check
+	 * Check the token send by post
+	 * @param string $name Form name (optional)
+	 * @return bool Correct token or not
+	 */
+	static function check($name = 'default') {
+		if(!isset($_SESSION['csrf'][$name])):	
+			return false;
+		endif;
+		
+		if(!isset($_POST['CSRFtoken_'.$name]) || $_POST['CSRFtoken_'.$name] != $_SESSION['csrf'][$name]):	
+			return false;
+		endif;
+		
+		return true;
+	}
+}
