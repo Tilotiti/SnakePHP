97b002ca4c7b65e50fba606515236ebcb14341fa
Thibault Henry <thibaulthenry@tilotiti.com>
2014-07-25 10:39:16 +0200

    New session class

--- lib/plugin/session/plugin.session.php
+++ lib/plugin/session/plugin.session.php
@@ -1,27 +1,70 @@
 <?php
-/*
- * Plugin : User
- * Description : Gestion d'une base de donnée utilisateur et de leurs actions sur le site
- * Auteur : Thibault HENRY
- * Site : http://www.tiloweb.com
- */
-
-// Détection de la dépendance au plugin user
-if(file_exists(PLUGIN.'/user/plugin.user.php')):
-
-    // Temps maximum d'une session avant rafraichissement des données automatique depuis la base de donnée
-    define('SESSION_REFRESH', 1200); 
-    
-    // Insertion des classes User et Session
-    require_once 'class.session.php'; // Gestion des sessions utilisateurs
-    
-    // Création de la session utilisateur par défault
-    if(!isset($_SESSION['user'])):
-    	$_SESSION['user'] = false;
-    endif;
-    
-else:
-    class session {}
-    debug::error('Plugin Session', 'The user plugin must be activated', __FILE__, __LINE__);
-endif;
-?>
+class session {
+	/**
+	 * Login
+	 * Load user information in session
+	 * @param int $id User ID
+	 * @return bool User found
+	 */
+	public static function login($id) {
+		$user = new user((int)$id);
+		if($user->ok()):
+			$_SESSION['user']  = $user->get();
+			$_SESSION['user']['login'] = time();
+			self::save();
+			return true;
+		else:
+			return false;
+		endif;
+	}
+	
+	/*
+	 * Ok
+	 * Return if the session is connected
+	 * @return bool session connected
+	 */
+	public static function ok() {
+		return !empty($_SESSION['user']['id']);
+	}
+	
+	/**
+	 * Get
+	 * Find a session field
+	 * @param string $key Field's name
+	 * @return mixed Field's value
+	 */
+	public static function get($key) {
+		if(!empty($_SESSION['user'][$key])):			
+			return $_SESSION['user'][$key];
+		else:
+			return false;
+		endif;
+	}
+	
+	/**
+	 * Set
+	 * Set a new value to a field
+	 * @param string $key Field's name
+	 * @param mixed $value Field's new value
+	 */
+	public static function set($key, $value) {
+		$_SESSION['user'][$key] = $value;
+	}
+	
+	/**
+	 * Save
+	 * Save the session in the database
+	 */
+	public static function save() {
+		$user = new user($_SESSION['user']);
+		$user->save();
+	}
+	
+	/*
+	 * Logout
+	 * Disconnect the session
+	 */
+	public static function logout() {
+		unset($_SESSION['user']);
+	}
+}
